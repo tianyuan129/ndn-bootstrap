093dda19a96eace8b742cba80817d8f7177d271a
Tianyuan Yu <tianyuan@cs.ucla.edu>
2022-04-18 22:36:30 -0700

    docs: fix conf.py bug

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../src'))
 
-from ndn import __version__
+from bootstrap import __version__
 
 # -- Project information -----------------------------------------------------
 
